3a4fcbe19f69a5e44f824dbce29094ef6f7f30dc
Vladislav Pyatnitskiy <111236801+vladislavpyatnitskiy@users.noreply.github.com>
2023-09-13 10:26:22 +0500

    Update Descriptive_Stats_matrix.R
    Modified script so now it shows more stats, such as min, 10%, 90% and max

--- Descriptive_Stats_matrix.R
+++ Descriptive_Stats_matrix.R
@@ -1,39 +1,51 @@
-stats_for_sec <- function(x){
-  # Get logs of prices
-  lrtn=diff(log(x))
-  lrtn <- lrtn[-1,]
-  
-  # Calculate necessary statistics
-  stats1 <- t(apply(lrtn,
-                    2,
-                    function(lrtn) c(mean(lrtn),
-                                     quantile(lrtn,
-                                              na.rm = T,
-                                              probs = c(0.25)
-                                     ),
-                                     median(lrtn),
-                                     quantile(lrtn,
-                                              na.rm = T,
-                                              probs = c(0.75)
-                                     ),
-                                     sd(lrtn),
-                                     skewness(lrtn),
-                                     kurtosis(lrtn)
-                    )
-  )
-  )
-  
-  # Create colnames for matrix
-  colnames(stats1) <- c("Mean",
-                        "25%",
-                        "50%",
-                        "75%",
-                        "SD",
-                        "Skewness",
-                        "Kurtosis")
-  # Return output
-  return(stats1)
-  
+stat_for_assets <- function(x){
+    # Calculate log returns and remove NA
+    x=diff(log(x))[-1,]
+    
+    # Calculate necessary statistics
+    nec_stats <- t(apply(x,
+                      2,
+                      function(x) c(min(x),
+                                       quantile(x,
+                                                na.rm = T,
+                                                probs = c(0.1)
+                                       ),
+                                       quantile(x,
+                                                na.rm = T,
+                                                probs = c(0.25)
+                                       ),
+                                       median(x),
+                                       quantile(x,
+                                                na.rm = T,
+                                                probs = c(0.75)
+                                       ),
+                                       quantile(x,
+                                                na.rm = T,
+                                                probs = c(0.9)
+                                       ),
+                                       max(x),
+                                       mean(x),
+                                       sd(x),
+                                       skewness(x),
+                                       kurtosis(x)
+                      )
+    )
+    )
+    
+    # Create colnames for matrix
+    colnames(nec_stats) <- c("Min",
+                          "10%",
+                          "25%",
+                          "50%",
+                          "75%",
+                          "90%",
+                          "Max",
+                          "Mean",
+                          "SD",
+                          "Skewness",
+                          "Kurtosis")
+    # Return output
+    return(nec_stats)
 }
-
-stats_for_sec(portfolioReturns)
+# Test
+stat_for_assets(stock_data)
