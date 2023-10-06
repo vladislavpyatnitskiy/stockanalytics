ab8098800570ce62d91901ffea5f8ef905ee4996
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-06 22:55:27 +0500

    Update Sharpe Ratio.R
    Optimised script:
    
    • Reduced excessive script
    • Merged steps, such as calculation of mean and standard deviation
    • Added option to choose risk free security, e.g. gilts or other governmental bonds available in Yahoo! Finance

--- Securities & Portfolio Performance/Sharpe Ratio.R
+++ Securities & Portfolio Performance/Sharpe Ratio.R
@@ -4,102 +4,77 @@
 library(quantmod)
 library(timeSeries)
 
-sharpe_ratio <- function(y, z = NULL, i = NULL){
-  # Define Yahoo ticker for 10 year Treasuries
-  tr10 <- "^TNX"
-    
+sharpe_ratio <- function(y, tr10 = "^TNX", start_date = NULL, end_date = NULL){
+  
   # Add 10 year Treasuries to list
   y <- c(y, tr10)
-    
+  
   # Create an empty variable
   portfolioPrices <- NULL
-    
+  
   # Loop for data extraction
   for (Ticker in y){
+    
     # Set up statements for start and end dates
-    if (is.null(z) && is.null(i)) {
+    if (is.null(start_date) && is.null(end_date)) {
+      
       # When neither start date nor end date are defined
       portfolioPrices <- cbind(portfolioPrices,
-                                getSymbols(Ticker,
+                               getSymbols(Ticker,
                                           from = as.Date(Sys.Date()) - 365,
                                           to = Sys.Date(),
                                           src = "yahoo",
-                                          auto.assign=FALSE)[,4])
-    } else if (is.null(i)) {
-      # When only start date is defined
-      portfolioPrices <- cbind(portfolioPrices,
-                                getSymbols(Ticker, from = z, src = "yahoo",
-                                          auto.assign=FALSE)[,4])
-    } else if (is.null(z)) {
-      # When only end date is defined
-      portfolioPrices <- cbind(portfolioPrices,
-                                getSymbols(Ticker, to = i, src = "yahoo",
-                                          auto.assign=FALSE)[,4])
-    } else { 
+                                          auto.assign=FALSE)[,4]) } else { 
+                                            
       # When both start date and end date are defined
       portfolioPrices <- cbind(portfolioPrices,
-                                getSymbols(Ticker, from = z, to = i,
+                               getSymbols(Ticker, from = start_date,
+                                          to = end_date,
                                           src = "yahoo", 
-                                          auto.assign=FALSE)[,4])
-      }
-  }
+                                          auto.assign=FALSE)[,4]) } }
   # Get rid of NAs
   portfolioPrices <- portfolioPrices[apply(portfolioPrices,1,
-                                             function(x) all(!is.na(x))),]
+                                           function(x) all(!is.na(x))),]
   # Put the tickers in data set
   colnames(portfolioPrices) <- y
-    
+  
   # Make data discrete
   portfolioReturns <- ROC(portfolioPrices, type = "discrete")
-    
+  
   # Make it time series
   portfolioReturns <-as.timeSeries(portfolioPrices)
   
-  # Subtract treasuries from matrix
-  treasuries <- (portfolioReturns[,ncol(portfolioReturns)])
-  
   # Calculate risk free return
-  treasuries <- apply(treasuries,
-                      2,
-                      function(col) mean(diff(log(col))))
+  treasuries <- apply(portfolioReturns[,tr10],
+                      2, function(col) mean(diff(log(col))))
   
   # Form matrix for other financial instruments
-  security_prices <- portfolioReturns[,1:(ncol(portfolioReturns)-1)]
+  security_prices <- portfolioReturns[,-which(names(portfolioReturns) == tr10)]
   
-  # Calculate Standard Deviation
-  sd_calculated <- apply(security_prices,
-                         2,
-                         function(col) sd(diff(log(col))))
+  # Turn to logs
+  security_prices <- diff(log(security_prices))[-1,]
   
-  # Calculate return
-  mean_calculated <- apply(security_prices,
-                           2,
-                           function(col) mean(diff(log(col))))
+  # calculate mean and standard deviation
+  params_clcd <- apply(security_prices, 2, function(x) c(mean(x), sd(x)))
   
   # Define new list name to contain Sharpe values
   df_sharpe <- NULL
   
   # Calculate Sharpe ratio
-  for (k in 1:(ncol(security_prices))){
-    final_sharpe_coef <- ((mean_calculated[k] - treasuries) /
-      sd_calculated[k]) * (252^0.5)
+  for (k in 1:ncol(security_prices)){
     
     # Put values into list
-    df_sharpe <- cbind(df_sharpe, final_sharpe_coef)
-  }
-  
+    df_sharpe <- rbind(df_sharpe, (params_clcd[1,k]-treasuries)*(252^0.5) /
+                         params_clcd[2,k]) }
   # Add tickers
-  colnames(df_sharpe) <- tickers
+  rownames(df_sharpe) <- tickers
   
   # Put name of ratio to column
-  rownames(df_sharpe) <- "Sharpe"
-  
-  # Transpose
-  df_sharpe <- t(df_sharpe)
+  colnames(df_sharpe) <- "Sharpe"
   
   # Display values
   return(df_sharpe)
 }
-# Test 
+# Test
 tickers <- c("STLA", "UNM", "NVDA", "AAPL", "CVNA")
-sharpe_ratio(tickers, "2022-09-01")
+sharpe_ratio(tickers, start_date = "2022-09-01")
