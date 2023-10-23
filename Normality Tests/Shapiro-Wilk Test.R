d8e125add7f053fab02fc6a9b35487c83c08f46d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-23 19:44:55 +0500

    Update Shapiro-Wilk Test.R
    Optimised script: renamed variables & combined operations

--- Normality Tests/Shapiro-Wilk Test.R
+++ Normality Tests/Shapiro-Wilk Test.R
@@ -1,23 +1,20 @@
-# Function for Shapiro Test
-test_for_shapiro <- function(x, lg = F){
+# Shapiro Test
+t.shapiro <- function(x, lg = F){
   
-  #Calculate logs and remove NA if needed
+  # Calculate logs and remove NA if needed
   if (isTRUE(lg)){ x = diff(log(x))[-1,] }
   
   # Create place to store values
-  table_for_shapiro <- NULL
+  table <- NULL
   
   # For each column find values of Shapiro and store them into matrix
-  for (n in 1:ncol(x)){ table_for_shapiro <- cbind(table_for_shapiro,
-                                                   shapiro.test(x[,n])) }
-  # Give it column names from original matrix
-  colnames(table_for_shapiro) <- colnames(x)
+  for (n in 1:ncol(x)){ table <- cbind(table, shapiro.test(x[,n])) }
   
-  # Get rid of excess rows
-  table_for_shapiro <- table_for_shapiro[1:(nrow(table_for_shapiro)-2),]
+  # Give it column names from original matrix
+  colnames(table) <- colnames(x)
   
   # Display values
-  return(table_for_shapiro)
+  return(table[1:(nrow(table)-2),])
 }
 # Test
-test_for_shapiro(portfolioReturns, lg = T)
+t.shapiro(stock_data, lg = T)
