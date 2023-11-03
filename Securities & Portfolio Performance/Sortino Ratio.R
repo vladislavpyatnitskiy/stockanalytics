536dc31807fa1163f855e4b75849942d45ae160b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-03 19:32:13 +0500

    Update Sortino Ratio.R
    Debugged & Upgraded script:
    
    • Fixed issue with data upload
    • Reduced excessive lines

--- Securities & Portfolio Performance/Sortino Ratio.R
+++ Securities & Portfolio Performance/Sortino Ratio.R
@@ -1,40 +1,36 @@
 # Libraries
-library(quantmod)
-library(timeSeries)
+lapply(c("quantmod", "timeSeries"), require, character.only = TRUE)
 
 # Function to calculate Sortino Ratio
-sortino.ratio <- function(y,tr10 = "^TNX",z = NULL,i = NULL,v.sortino = NULL){
+sortino.ratio <- function(y,tr10 = "^TNX",s = NULL,e = NULL,v.sortino = NULL){
   
   y <- c(y, tr10) # Add 10 year Treasuries to list
   
-  p.Prices <- NULL # Create an empty variable
+  p <- NULL # Create an empty variable
   
-  # Loop for data extraction and set up statements for start and end date
-  for (Ticker in y){ if (is.null(start_date) && is.null(end_date)) {
-      
-      # When neither start date nor end date are defined
-      p.Prices <- cbind(p.Prices,
-                        getSymbols(Ticker,
-                                   from = as.Date(Sys.Date()) - 365,
-                                   to = Sys.Date(),
-                                   src = "yahoo",
-                                   auto.assign=FALSE)[,4]) } else { 
-                                            
-      # When both start date and end date are defined
-      p.Prices <- cbind(p.Prices,
-                        getSymbols(Ticker, 
-                                   from = start_date,
-                                   to = end_date,
-                                   src = "yahoo", 
-                                   auto.assign=FALSE)[,4]) } }
-  # Get rid of NAs
-  p.Prices <- p.Prices[apply(p.Prices,1,function(x) all(!is.na(x))),]
+  for (Ticker in y){ if (is.null(s) && is.null(e)) { # Data upload
+    
+    # When neither start date nor end date are defined
+    p<-cbind(p,getSymbols(Ticker,from=as.Date(Sys.Date())-365,to=Sys.Date(),
+                          src="yahoo",auto.assign=F)[,4])
+    
+  } else if (is.null(e)) { # When only start date is defined
+    
+    p <- cbind(p, getSymbols(Ticker,from=s,src="yahoo",auto.assign=F)[,4])
+    
+  } else if (is.null(s)) { # When only end date is defined
+    
+    p <- cbind(p, getSymbols(Ticker,to=e,src = "yahoo",auto.assign=F)[,4])
+    
+  } else { # When both start date and end date are defined
+    
+    p<-cbind(p,getSymbols(Ticker,from=s,to=e,src="yahoo",auto.assign=F)[,4])} }
   
-  colnames(p.Prices) <- y # Put the tickers in data set
+  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
-  r <- ROC(p.Prices, type = "discrete") # Make data discrete
+  colnames(p) <- y # Put the tickers in data set
   
-  r <- diff(log(as.timeSeries(p.Prices)))[-1,] # Clean data for returns
+  r <- diff(log(as.timeSeries(p)))[-1,] # Clean data for returns
   
   rf <- apply(r[,tr10], 2, function(col) mean(col)) # Risk Free Rate
   
@@ -50,13 +46,12 @@ sortino.ratio <- function(y,tr10 = "^TNX",z = NULL,i = NULL,v.sortino = NULL){
     n.r <- (r.premium[r.premium < 0]) ^ 2 # Negative excessive returns and ^2
     
     # Calculate Downside Risk, Sortino ratio and add to list
-    v.sortino<-rbind(v.sortino, ((e.r-rf)/(sum(n.r)/nrow(s))^(0.5))*252^0.5) }
+    v.sortino<-rbind(v.sortino, ((e.r - rf)/(sum(n.r)/nrow(s))^.5) * 252^.5) }
     
   rownames(v.sortino) <- colnames(s) # Add tickers
-  
   colnames(v.sortino) <- "Sortino" # Put name of ratio to column
   
   return(v.sortino) # Display values
 }
 # Test
-sortino.ratio(c("STLA", "UNM", "NVDA", "AAPL", "CVNA"), z = "2022-09-01")
+sortino.ratio(c("STLA", "UNM", "NVDA", "AAPL", "CVNA"), s = "2022-09-01")
