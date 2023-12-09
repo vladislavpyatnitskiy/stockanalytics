497e677bcf099076b63dd1a7fc405378a7bd1701
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-09 20:40:34 +0500

    Update Shapiro-Wilk Test.R
    moved lines

--- Normality Tests/Shapiro-Wilk Test.R
+++ Normality Tests/Shapiro-Wilk Test.R
@@ -7,11 +7,10 @@ t.shapiro <- function(x, lg = F, table = NULL){
   # For each column find values of Shapiro and store them into matrix
   for (n in 1:ncol(x)){ table <- cbind(table, shapiro.test(x[,n])) }
   
-  # Give it column names from original matrix
-  colnames(table) <- colnames(x)
-  
-  # Display values
-  return(table[1:(nrow(table)-2),])
+    # Give it column names from original matrix
+    colnames(table) <- colnames(x)
+    
+    # Display values
+    return(table[1:(nrow(table)-2),])
 }
-# Test
-t.shapiro(stock_data, lg = T)
+t.shapiro(stock_data, lg = T) # Test
