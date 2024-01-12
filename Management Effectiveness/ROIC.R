f494a091b04d3dab444001c4285aba40d4627ffe
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-12 22:44:13 +0500

    Update ROIC.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined functions

--- Management Effectiveness/ROIC.R
+++ Management Effectiveness/ROIC.R
@@ -19,28 +19,19 @@ ROIC <- function(x){ # Return on Invested Capital
     u <- tab.is %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
     
     c <- NULL
-    h <- NULL
-    
-    p <- c("Invested Capital")
     
     r <- c("Operating Income","Tax Provision","Net Income Common Stockholders")
     
-    for (m in 1:length(r)){ q <- NULL
-    
-      for (n in seq(1)){ q <- cbind(q, u[grep(r[m], u) + n])
+    for (m in 1:length(r)){ q <- u[grep(r[m], u) + 1]
       
       o <- NULL
       
-      if (length(q) > 1){  o <- c(o, q[1]) 
-      
-        } else if (length(q) == 1) { o <- q } } 
+      if (length(q) > 1){  o <- c(o,q[1]) } else if (length(q) == 1) { o <- q }  
       
       c <- rbind(c, o) }
       
     c <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", c)) # Reduce commas
-    h <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", y[grep(p, y) + 1])) 
-    
-    rownames(c) <- r # Give dates
+    h<-gsub(",","",gsub("([a-zA-Z]),","\\1 ",y[grep("Invested Capital",y)+1])) 
     
     oi <- as.numeric(c[1]) # Operating Income
     tp <- as.numeric(c[2]) # Tax Provision
@@ -48,7 +39,7 @@ ROIC <- function(x){ # Return on Invested Capital
     ic <- as.numeric(h) # Investing Capital
     
     roic <- rbind(roic, (1 - tp / ni) * oi / as.numeric(h)) } # Add values
-  
+    
   rownames(roic) <- x # Ticker names
   colnames(roic) <- "ROIC (%)" # Column Name
   
