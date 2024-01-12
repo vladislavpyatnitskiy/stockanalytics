1f397a920854c9a1ddcbfa2c5640925fd754d7c8
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-12 22:34:00 +0500

    Update ROA.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined functions

--- Management Effectiveness/ROA.R
+++ Management Effectiveness/ROA.R
@@ -6,42 +6,24 @@ ROA <- function(x){ # Return on Assets
   
   for (q in 1:length(x)){ a <- x[q] # Each ticker in vector
   
-  bs <- sprintf("https://finance.yahoo.com/quote/%s/balance-sheet?p=%s",a,a)
-  is <- sprintf("https://finance.yahoo.com/quote/%s/financials?p=%s", a, a)
-  
-  page.bs <- read_html(bs) # Read HTML & extract necessary info
-  page.is <- read_html(is) # Read HTML & extract necessary info
-  
-  price.yahoo1 <- page.bs %>% html_nodes('div') %>% .[[1]] -> tab.bs
-  price.yahoo2 <- page.is %>% html_nodes('div') %>% .[[1]] -> tab.is
-  
-  y <- tab.bs %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
-  u <- tab.is %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
-  
-  c <- NULL
-  h <- NULL
-  
-  p <- c("Total Assets")
-  
-  r <- c("EBIT")
-  
-  for (m in 1:length(r)){ q <- NULL
-  
-    for (n in seq(1)){ q <- cbind(q, u[grep(r[m], u) + n])
+    bs <- sprintf("https://finance.yahoo.com/quote/%s/balance-sheet?p=%s",a,a)
+    is <- sprintf("https://finance.yahoo.com/quote/%s/financials?p=%s", a, a)
     
-    o <- NULL
+    page.bs <- read_html(bs) # Read HTML & extract necessary info
+    page.is <- read_html(is) # Read HTML & extract necessary info
     
-    if (length(q) > 1){  o <- c(o, q[1]) 
+    price.yahoo1 <- page.bs %>% html_nodes('div') %>% .[[1]] -> tab.bs
+    price.yahoo2 <- page.is %>% html_nodes('div') %>% .[[1]] -> tab.is
     
-      } else if (length(q) == 1) { o <- q } } 
+    y <- tab.bs %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
+    u <- tab.is %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
     
-    c <- rbind(c, o) }
+    # Take values for EBIT and Total Assets, clean them and make numeric
+    c <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", u[grep("EBIT", u) + 1][1])) 
+    h <- gsub(",", "", gsub("([a-zA-Z]),","\\1 ",y[grep("Total Assets",y)+1])) 
     
-  c <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", c)) # Reduce commas
-  h <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", y[grep(p, y) + 1])) 
-  
-  roa <- rbind(roa, as.numeric(c) / as.numeric(h)) } # EBIT / Total Assets
-  
+    roa <- rbind(roa, as.numeric(c) / as.numeric(h)) } # EBIT / Total Assets
+    
   rownames(roa) <- x # Ticker names
   colnames(roa) <- "ROA (%)" # Column Name
   
