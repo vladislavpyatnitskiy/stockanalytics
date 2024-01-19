f53ef4d9176bab6a4bd15b3e15166e9db4a743fe
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-19 15:01:54 +0500

    Create Mean Square Error.R
    Added script to calculate Mean Square Error and Root Mean Square Error

---
+++ Statistics/Mean Square Error.R
@@ -0,0 +1,44 @@
+lapply(c("quantmod", "timeSeries"), require, character.only = T) # libraries
+
+MSE <- function(x, s = NULL, e = NULL, data = T, root = T){ # MSE
+  
+  if (isTRUE(data)){ p <- NULL # Create an empty variable
+  
+    # Loop for data extraction & # Set up statements for start and end dates
+    for (A in x){ if (is.null(s) && is.null(e)) {
+      
+      # When neither start date nor end date are defined
+      p <- cbind(p, getSymbols(A, src = "yahoo", auto.assign=F)[,4])
+      
+    } else if (is.null(e)) { # When only start date is defined
+      
+      p <- cbind(p, getSymbols(A, from = s,src="yahoo",auto.assign=F)[,4])
+      
+    } else if (is.null(s)) { # When only end date is defined
+      
+      p <- cbind(p,getSymbols(A, to = e, src = "yahoo", auto.assign = F)[,4])
+      
+    } else { # When both start date and end date are defined
+      
+      p<-cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4])}
+    }
+    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+    
+    colnames(p) <- x # Put the tickers in data set
+    
+    x <- as.timeSeries(p) } # Make it time series
+    
+    l <- NULL
+    
+    for (n in 1:ncol(x)){ s <- x[,n]
+    
+      if (isTRUE(root)){ l <- rbind(l, (sum((s-mean(s))^2)/length(s))^.5) }
+    
+      else { l <- rbind(l, (sum((s - mean(s)) ^ 2) / length(s))) } }
+  
+  rownames(l) <- colnames(x)
+  if (isTRUE(root)){ colnames(l) <- "RMSE" } else { colnames(l) <- "MSE" }
+  
+  l # Display
+}
+MSE(c("AAPL", "C"), s = "2020-01-01", data = T, root = F) # Test
