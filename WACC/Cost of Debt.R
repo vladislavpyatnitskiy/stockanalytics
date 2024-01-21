257bf568ea5d958e1c46afad6ef29da136b2a997
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-21 13:30:27 +0500

    Update Cost of Debt.R
    Optimised script:
    
    • Reduced excessive lines
    • Rewrote FOR loops

--- WACC/Cost of Debt.R
+++ WACC/Cost of Debt.R
@@ -22,32 +22,10 @@ cost.of.debt <- function(x){ # Cost of Debt
   
   r <- c("Interest Expense", "Tax Provision", "Net Income Common Stockholders")
   
-  for (m in 1:length(r)){ q <- NULL
+  for (m in 1:length(r)){ c <- rbind(c, u[grep(r[m], u) + 1][1]) }
+  for (m in 1:length(p)){ h <- rbind(h, y[grep(p[m], y) + 1][1])  }
   
-    for (n in seq(1)){ q <- cbind(q, u[grep(r[m], u) + n])
-    
-    o <- NULL
-    
-    if (length(q) > 1){  o <- c(o, q[1]) 
-    
-      } else if (length(q) == 1) { o <- q } } 
-    
-    c <- rbind(c, o) }
-    
   c <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", c)) # Income Statement
-  
-  for (m in 1:length(p)){ v <- NULL
-  
-    for (n in seq(1)){ v <- cbind(v, y[grep(p[m], y) + n])
-    
-    w <- NULL
-    
-    if (length(v) > 1){ for (n in seq(0,3,1)) w <- c(w, v[1 + 2*n]) 
-    
-      } else if (length(v) == 1) { w <- v } } 
-    
-    h <- rbind(h, w) }
-    
   h <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", h)) # Balance Sheet
   
   Rd <- as.numeric(c[1]) / as.numeric(h[1]) # Interest Expense / Total Debt
