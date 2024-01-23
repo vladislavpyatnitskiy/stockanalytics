f5c064d6012e4a39c1b4e1ab4f4b7c4f37345ff6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-23 17:12:54 +0500

    Update Cost of Debt.R
    Upgraded script:
    
    • Added option to get cost of debt values for several securities simultaneously 
    • Combined AS.NUMERIC function with the one that reduces commas from data

--- WACC/Cost of Debt.R
+++ WACC/Cost of Debt.R
@@ -2,40 +2,45 @@ library("rvest") # Library
 
 cost.of.debt <- function(x){ # Cost of Debt
   
-  bs <- sprintf("https://finance.yahoo.com/quote/%s/balance-sheet?p=%s", x, x)
-  is <- sprintf("https://finance.yahoo.com/quote/%s/financials?p=%s", x, x)
+  l <- NULL
   
-  page.bs <- read_html(bs) # Read HTML & extract necessary info
-  page.is <- read_html(is) # Read HTML & extract necessary info
+  for (n in 1:length(x)){ a <- x[n]
   
-  price.yahoo1 <- page.bs %>% html_nodes('div') %>% .[[1]] -> tab.bs
-  price.yahoo2 <- page.is %>% html_nodes('div') %>% .[[1]] -> tab.is
+    bs <- sprintf("https://finance.yahoo.com/quote/%s/balance-sheet?p=%s",a,a)
+    is <- sprintf("https://finance.yahoo.com/quote/%s/financials?p=%s", a, a)
+    
+    page.bs <- read_html(bs) # Read HTML & extract necessary info
+    page.is <- read_html(is) # Read HTML & extract necessary info
+    
+    price.yahoo1 <- page.bs %>% html_nodes('div') %>% .[[1]] -> tab.bs
+    price.yahoo2 <- page.is %>% html_nodes('div') %>% .[[1]] -> tab.is
+    
+    y <- tab.bs %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
+    u <- tab.is %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
+    
+    c <- NULL
+    h <- NULL
+    
+    p <- c("Total Debt", "Total Liabilities Net Minority Interest",
+           "Total Equity Gross Minority Interest")
+    
+    r <- c("Interest Expense","Tax Provision","Net Income Common Stockholders")
+    
+    for (m in 1:length(r)){ c <- rbind(c, u[grep(r[m], u) + 1][1]) }
+    for (m in 1:length(p)){ h <- rbind(h, y[grep(p[m], y) + 1][1])  }
+    
+    c<-as.numeric(gsub(",","",gsub("([a-zA-Z]),","\\1 ",c))) # Income Statement
+    h<-as.numeric(gsub(",","",gsub("([a-zA-Z]),","\\1 ",h))) # Balance Sheet
+    
+    Rd <- c[1] / h[1] # Interest Expense / Total Debt
+    after.tax.ratio <- 1 - c[2] / c[3] # Tax Provision / Net Income 
+    debt.part <- h[2] / (h[2] + h[3]) # Liabilities / Total Assets
+    
+    l <- rbind.data.frame(l, debt.part * Rd * after.tax.ratio) } # Display
   
-  y <- tab.bs %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
-  u <- tab.is %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
+  rownames(l) <- x
+  colnames(l) <- "Cost of Debt"
   
-  c <- NULL
-  h <- NULL
-  
-  p <- c("Total Debt", "Total Liabilities Net Minority Interest",
-         "Total Equity Gross Minority Interest")
-  
-  r <- c("Interest Expense", "Tax Provision", "Net Income Common Stockholders")
-  
-  for (m in 1:length(r)){ c <- rbind(c, u[grep(r[m], u) + 1][1]) }
-  for (m in 1:length(p)){ h <- rbind(h, y[grep(p[m], y) + 1][1])  }
-  
-  c <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", c)) # Income Statement
-  h <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", h)) # Balance Sheet
-  
-  Rd <- as.numeric(c[1]) / as.numeric(h[1]) # Interest Expense / Total Debt
-  
-  # Tax Provision / Net Income Common Stockholders
-  after.tax.ratio <- 1 - as.numeric(c[2]) / as.numeric(c[3]) 
-  
-  # Liabilities / Total Assets
-  debt.part <- as.numeric(h[2]) / (as.numeric(h[2]) + as.numeric(h[3]))
-  
-  debt.part * Rd * after.tax.ratio # Display
+  l # Display data frame
 }
-cost.of.debt("AAPL") # Test
+cost.of.debt(c("AAPL", "MSFT", "NVDA", "TSLA")) # Test
