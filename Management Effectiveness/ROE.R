5dcb09dc303cbd26bb57a29f99258479c6917ea4
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-25 15:03:55 +0500

    Update ROE.R
    Updated link

--- Management Effectiveness/ROE.R
+++ Management Effectiveness/ROE.R
@@ -5,9 +5,9 @@ ROE <- function(x){ # Return on Equity
   roe <- NULL # List for ROE values
   
   for (q in 1:length(x)){ a <- x[q] # Each ticker in vector
-  
-    bs <- sprintf("https://finance.yahoo.com/quote/%s/balance-sheet?p=%s",a,a)
-    is <- sprintf("https://finance.yahoo.com/quote/%s/financials?p=%s", a, a)
+    
+    bs<-sprintf("https://uk.finance.yahoo.com/quote/%s/balance-sheet?p=%s",a,a)
+    is <- sprintf("https://uk.finance.yahoo.com/quote/%s/financials?p=%s",a,a)
     
     page.bs <- read_html(bs) # Read HTML & extract necessary info
     page.is <- read_html(is) # Read HTML & extract necessary info
@@ -19,12 +19,10 @@ ROE <- function(x){ # Return on Equity
     u <- tab.is %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
     
     # Take values for Net Income and Equity
-    c <- gsub(",","",gsub("([a-zA-Z]),","\\1 ",
-                          u[grep("Net Income Common Stockholders",u)+1][1]))
+    c <- gsub(",","",gsub("([a-zA-Z]),","\\1 ", u[grep("Net income",u)+1][1]))
     
     h <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ",
-                            y[grep("Total Equity Gross Minority Interest",
-                                   y) + 1])) 
+                            y[grep("Total stockholders' equity", y) + 1])) 
     
     roe <- rbind(roe, as.numeric(c) / as.numeric(h)) } # Net Income / Equity
     
