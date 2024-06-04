32353ef8422c77a65c0e675fc0349b3fdd008f8a
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-06-04 10:52:36 +0500

    Update Beta calculation.R
    Optimised script:
    
    • Fixed time periods
    • Added option to get alpha values

--- Systematic Risk (Beta)/Beta calculation.R
+++ Systematic Risk (Beta)/Beta calculation.R
@@ -1,36 +1,39 @@
 # Libraries 
 lapply(c("quantmod", "timeSeries"), require, character.only = TRUE)
 
-# Function to generate beta values
-c.beta <- function(y, bnchm = "^GSPC", s = NULL, e = NULL){
+c.beta <- function(y, i = "^GSPC", c=2, s = NULL, e = NULL){ # Beta || Alpha
   
-  y <- c(y, bnchm) # Add benchmark to list
+  y <- c(y, i) # Add benchmark to list
   
   p <- NULL # Create an empty variable
   
-  # Loop for data extraction & Set up statements for start and end dates
-  for (Ticker in y){ if (is.null(s) && is.null(e)) {
+  for (A in y){ if (is.null(s) && is.null(e)) { # When Time Period not defined
     
-      # When neither start date nor end date are defined
-      p<-cbind(p,getSymbols(Ticker,from=as.Date(Sys.Date())-365,to=Sys.Date(),
-                            src = "yahoo", auto.assign=F)[,4]) } else { 
-                                 
-      # When both start date and end date are defined
-      p <- cbind(p, getSymbols(Ticker, from = s, to = e, src = "yahoo",
-                               auto.assign=F)[,4]) } }
+      p <- cbind(p, getSymbols(A, src = "yahoo", auto.assign = F)[,4])
+    
+    } else if (is.null(e)) { # When only start date is defined
+    
+      p <- cbind(p,getSymbols(A, from = s, src = "yahoo", auto.assign = F)[,4])
+    
+    } else if (is.null(s)) { # When only end date is defined
+    
+      p <- cbind(p, getSymbols(A, to = e, src = "yahoo", auto.assign = F)[,4])
+    
+    } else { # When both start date and end date are defined
+    
+      p <- cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4]) } }
   
-  p <- p[apply(p,1, function(x) all(!is.na(x))),] # Get rid of NA
+  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
   colnames(p) <- y # Put the tickers in data set
   
   x=diff(log(as.timeSeries(p)))[-1,] # Calculate Returns and get rid of NA
   
-  beta <- as.matrix(apply(x[, -which(names(x) == bnchm)], 2, # Calculate Beta
-                    function(col) ((lm((col) ~ x[,bnchm]))$coefficients[2])))
+  beta <- as.matrix(apply(x[, -which(names(x) == i)], 2, # Calculate Beta
+                          function(col) ((lm((col) ~ x[,i]))$coefficients[c])))
   
-  colnames(beta) <- "Beta" # Name column 
+  if (c == 2){ colnames(beta) <- "Beta" } else { colnames(beta) <- "Alpha" }
   
   return(beta) # Display values
 }
-# Test
-c.beta(tickers_for_test, bnchm = "^GSPC")
+c.beta(tickers_for_test, i = "^GSPC", c = 2) # Test
