502d884833b9c71b7aa9adb613fe560bc0434c4b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-16 10:30:14 +0500

    Update Beta calculation.R
    Optimised script:
    
    • Rewrote part for data extraction 
    • Reduced excessive lines

--- Systematic Risk (Beta)/Beta calculation.R
+++ Systematic Risk (Beta)/Beta calculation.R
@@ -1,39 +1,34 @@
-# Libraries 
-lapply(c("quantmod", "timeSeries"), require, character.only = TRUE)
+lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libraries 
 
-c.beta <- function(y, i = "^GSPC", c=2, s = NULL, e = NULL){ # Beta || Alpha
+c.beta <- function(y, i = "^GSPC", beta=T, s=NULL, e=NULL){ # Beta or Alpha
   
   y <- c(y, i) # Add benchmark to list
   
   p <- NULL # Create an empty variable
   
-  for (A in y){ if (is.null(s) && is.null(e)) { # When Time Period not defined
-    
-      p <- cbind(p, getSymbols(A, src = "yahoo", auto.assign = F)[,4])
-    
-    } else if (is.null(e)) { # When only start date is defined
-    
-      p <- cbind(p,getSymbols(A, from = s, src = "yahoo", auto.assign = F)[,4])
-    
-    } else if (is.null(s)) { # When only end date is defined
-    
-      p <- cbind(p, getSymbols(A, to = e, src = "yahoo", auto.assign = F)[,4])
-    
-    } else { # When both start date and end date are defined
-    
-      p <- cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4]) } }
+  src <- "yahoo"
+  
+  getData <- function(A, s, e) {
+    if (is.null(s) && is.null(e)) return(getSymbols(A, src=src, auto.assign=F)) 
+    if (is.null(e)) return(getSymbols(A, from = s, src=src, auto.assign=F)) 
+    if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
+    return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
+  }
+  for (A in y){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
   
   p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
   colnames(p) <- y # Put the tickers in data set
   
-  x=diff(log(as.timeSeries(p)))[-1,] # Calculate Returns and get rid of NA
+  x = diff(log(as.timeSeries(p)))[-1,] # Calculate Returns and get rid of NA
   
-  beta <- as.matrix(apply(x[, -which(names(x) == i)], 2, # Calculate Beta
-                          function(col) ((lm((col) ~ x[,i]))$coefficients[c])))
+  c = ifelse(beta == T, 2, 1)
   
-  if (c == 2){ colnames(beta) <- "Beta" } else { colnames(beta) <- "Alpha" }
+  B <- as.matrix(apply(x[, -which(names(x) == i)], 2,
+                       function(col) ((lm((col) ~ x[,i]))$coefficients[c])))
   
-  return(beta) # Display values
+  colnames(B) <- ifelse(beta == T, "Beta", "Alpha")
+  
+  return(B) # Display values
 }
-c.beta(tickers_for_test, i = "^GSPC", c = 2) # Test
+c.beta(tickers_for_test, i = "^GSPC", T) # Test
