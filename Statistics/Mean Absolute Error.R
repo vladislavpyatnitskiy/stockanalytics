1d439e197a27e7c91525258a0c0360346fb936b3
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-17 10:41:23 +0500

    Update Mean Absolute Error.R
    Optimised script:
    
    • Rewrote part for data extraction

--- Statistics/Mean Absolute Error.R
+++ Statistics/Mean Absolute Error.R
@@ -1,27 +1,19 @@
 lapply(c("quantmod", "timeSeries"), require, character.only = T) # libraries
 
-MAE <- function(x, s = NULL, e = NULL, data = T){ # Mean Absoulte Error
+MAE <- function(x, s = NULL, e = NULL, data = T){ # Mean Absolute Error
   
-  if (isTRUE(data)){ p <- NULL # Create an empty variable
+  if (data){ p <- NULL # Create an empty variable
     
-    # Loop for data extraction & # Set up statements for start and end dates
-    for (A in x){ if (is.null(s) && is.null(e)) {
-      
-      # When neither start date nor end date are defined
-      p <- cbind(p, getSymbols(A, src = "yahoo", auto.assign=F)[,4])
-      
-    } else if (is.null(e)) { # When only start date is defined
-      
-      p <- cbind(p, getSymbols(A, from = s,src="yahoo",auto.assign=F)[,4])
-      
-    } else if (is.null(s)) { # When only end date is defined
-      
-      p <- cbind(p,getSymbols(A, to = e, src = "yahoo", auto.assign = F)[,4])
-      
-    } else { # When both start date and end date are defined
-      
-      p<-cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4])}
+    src <- "yahoo"
+    
+    getData <- function(A, s, e) {
+      if (is.null(s) && is.null(e)) return(getSymbols(A,src=src,auto.assign=F)) 
+      if (is.null(e)) return(getSymbols(A, from = s, src=src, auto.assign=F)) 
+      if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
+      return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
     }
+    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
+    
     p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
     
     colnames(p) <- x # Put the tickers in data set
@@ -31,11 +23,12 @@ MAE <- function(x, s = NULL, e = NULL, data = T){ # Mean Absoulte Error
   l <- NULL
   
   for (n in 1:ncol(x)){ s <- x[,n]
-    
+  
     l <- rbind(l, sum(abs(s - mean(s))) / length(s)) }
-
+  
   rownames(l) <- colnames(x)
   colnames(l) <- "MAE"
+  
   l
 }
 MAE(c("AAPL", "C"), s = "2020-01-01", data = T) # Test
