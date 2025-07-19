f3946cdc934db0f0c1253714ed1a69b50523576d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-19 13:35:18 +0500

    Update Descriptive_Stats_matrix.R
    Optimised script:
    
    • Rewrote part for data extraction
    • Rewrote part for statistics ratios

--- Descriptive_Stats_matrix.R
+++ Descriptive_Stats_matrix.R
@@ -2,23 +2,17 @@ lapply(c("quantmod","timeSeries","fBasics"),require,character.only=T) # Libs
 
 fin.stats <- function(x, lg = F, data = F, transpose = F, s = NULL, e = NULL){ 
   
-  p <- NULL
+  if (data){ p <- NULL
+    
+    src="yahoo"
   
-  if (isTRUE(data)){ for (A in x){ if (is.null(s) && is.null(e)){
-    
-        q <- getSymbols(A, src = "yahoo", auto.assign=F)[,4]
-    
-        } else if (is.null(e)){ # When only start date is defined
-    
-        q <- getSymbols(A, from = s, src = "yahoo", auto.assign = F)[,4]
-    
-        } else if (is.null(s)){ 
-    
-        q <- getSymbols(A, to = e, src = "yahoo", auto.assign = F)[,4] } else { 
-      
-        q <- getSymbols(A, from=s, to=e, src="yahoo", auto.assign=F)[,4] } 
-    
-      p <- cbind(p, q) }
+    getData <- function(A, s, e) {
+      if (is.null(s) && is.null(e)) return(getSymbols(A,src=src,auto.assign=F)) 
+      if (is.null(e)) return(getSymbols(A, from = s, src=src, auto.assign=F)) 
+      if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
+      return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
+    }
+    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
     
     p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
     
@@ -26,20 +20,33 @@ fin.stats <- function(x, lg = F, data = F, transpose = F, s = NULL, e = NULL){
     
     x <- as.timeSeries(p) } # Make it time series
   
-  if (isTRUE(lg)){ x = diff(log(x))[-1,] } # logs & remove NA if needed
+  if (lg) x = diff(log(x))[-1,] # logs & remove NA if needed
   
   # Calculate necessary statistics
-  d <- t(apply(x, 2, function(x) c(min(x), quantile(x,na.rm=T,probs = c(.1)),
-                                   quantile(x, na.rm=T,probs=c(.25)),
-                                   median(x), quantile(x,na.rm=T,probs=c(.75)),
-                                   quantile(x,na.rm=T, probs = c(.9)), max(x),
-                                   mean(x), var(x), sd(x), skewness(x),
-                                   kurtosis(x))))
-  # Create column names for matrix
-  colnames(d) <- c("Min","10%","25%","Median 50%","75%", "90%", "Max",
-                   "Mean", "Variance", "SD", "Skewness", "Kurtosis")
+  d <- t(
+    apply(x, 2, function(x) c(
+      min(x),
+      quantile(x, na.rm=T, probs = c(.1)),
+      quantile(x, na.rm=T, probs = c(.25)),
+      median(x),
+      quantile(x, na.rm=T, probs = c(.75)),
+      quantile(x, na.rm=T, probs = c(.9)),
+      max(x),
+      mean(x),
+      var(x),
+      sd(x),
+      skewness(x),
+      kurtosis(x)
+      )
+      )
+    )
   
-  if (isTRUE(transpose)){ t(d) } else { d } # Display
+  # Create column names for matrix
+  colnames(d) <- c(
+    "Min", "10%", "25%", "Median 50%", "75%", "90%", "Max", "Mean", "Variance",
+    "SD", "Skewness", "Kurtosis")
+  
+  if (transpose){ t(d) } else { d } # Display
 }
 fin.stats(x = c("AMZN", "AAPL", "META", "GOOGL", "MSFT", "META", "NVDA",
                 "TSLA"), lg = T, data = T, transpose = T, s = "2023-01-01")
