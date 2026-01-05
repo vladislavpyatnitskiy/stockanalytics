47f82f42a5ea7634425d0d5f5ba22a37be9248a2
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-05 17:41:35 +0500

    Update Mean Absolute Error.R
    Added download message

--- Statistics/Mean Absolute Error.R
+++ Statistics/Mean Absolute Error.R
@@ -3,7 +3,7 @@ lapply(c("quantmod", "timeSeries"), require, character.only = T) # libraries
 MAE <- function(x, s = NULL, e = NULL, data = T){ # Mean Absolute Error
   
   if (data){ p <- NULL # Create an empty variable
-    
+  
     src <- "yahoo"
     
     getData <- function(A, s, e) {
@@ -12,19 +12,28 @@ MAE <- function(x, s = NULL, e = NULL, data = T){ # Mean Absolute Error
       if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
       return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
     }
-    for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
+  for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) 
+  
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        A, which(x == A), length(x)
+      )
+    ) # Download message
     
-    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
-    
-    colnames(p) <- x # Put the tickers in data set
-    
-    x <- as.timeSeries(p) } # Make it time series
-    
+  } # Join data
+  
+  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+  
+  colnames(p) <- x # Put the tickers in data set
+  
+  x <- as.timeSeries(p) } # Make it time series
+  
   l <- NULL
   
   for (n in 1:ncol(x)){ s <- x[,n]
   
-    l <- rbind(l, sum(abs(s - mean(s))) / length(s)) }
+  l <- rbind(l, sum(abs(s - mean(s))) / length(s)) }
   
   rownames(l) <- colnames(x)
   colnames(l) <- "MAE"
