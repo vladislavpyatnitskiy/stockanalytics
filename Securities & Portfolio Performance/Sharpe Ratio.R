0d0a79e74396b32fb8662fe1ed0668dff6310601
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-03-19 11:28:53 +0500

    Update Sharpe Ratio.R
    Optimised script:
    
    • Added download messages
    • Rewrote part for data extraction 
    • Renamed variables for better clarity

--- Securities & Portfolio Performance/Sharpe Ratio.R
+++ Securities & Portfolio Performance/Sharpe Ratio.R
@@ -1,15 +1,24 @@
 lapply(c("quantmod","timeSeries"),require,character.only=T) # Libraries
 
 # Script to calculate Sharpe Ratio
-sharpe.ratio <- function(y, tr = "^TNX", s = as.Date(Sys.Date())-365,
-                         e = as.Date(Sys.Date())){
+sharpe.ratio <- function(y, tr = "^TNX", s = NULL, e = NULL){
   
   y <- c(y, tr) # Add 10 year Treasuries to list
   
-  p <- NULL # Create an empty variable
+  p <- NULL # 4 scenarios: no dates, only start or end dates, both dates
+  src <- "yahoo"
   
-  for (A in y) p<-cbind(p,getSymbols(A,from=s,to=e,src="yahoo", # Daily data
-                                     auto.assign=F)[,4])
+  getData <- function(A, s, e) {
+    if (is.null(s) && is.null(e)) return(getSymbols(A, src=src, auto.assign=F)) 
+    if (is.null(e)) return(getSymbols(A, from = s, src=src, auto.assign=F)) 
+    if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
+    return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
+  }
+  for (A in y){ p <- cbind(p, getData(A, s, e)[,4]) 
+  
+    message(sprintf("%s is downloaded; %s from %s", A, which(y==A), length(y)))
+  
+  } # Join data
   
   p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
@@ -20,18 +29,19 @@ sharpe.ratio <- function(y, tr = "^TNX", s = as.Date(Sys.Date())-365,
   rf <- apply(p[,tr], 2, function(col) mean(col)) # Risk Free
   
   # Matrix of financial instruments & Turn to logs & Calculate Mean and SD
-  r<-apply(diff(log(p[,-which(names(p)==tr)]))[-1,],2,
-           function(col) c((exp(sum(col)) - 1) * 100, sd(col) * 1000))
+  r = apply(
+    diff(log(p[,-which(names(p) == tr)]))[-1,], 2,
+    function(col) c((exp(sum(col)) - 1) * 100, sd(col) * 1000)
+    )
   
-  sharpe <- NULL # List for Sharpe values
+  S <- NULL # List for Sharpe values
   
   # Calculate Sharpe ratio & Put values into list
-  for (k in 1:ncol(r)){ sharpe <- rbind(sharpe, (r[1,k] - rf) / r[2,k]) }
+  for (k in 1:ncol(r)) S <- rbind(S, (r[1,k] - rf) / r[2,k])
   
-  rownames(sharpe) <- colnames(r) # Add tickers
-  colnames(sharpe) <- "Sharpe" # Put name of ratio to column
+  rownames(S) <- colnames(r) # Add tickers
+  colnames(S) <- "Sharpe" # Put name of ratio to column
   
-  return(sharpe) # Display values
+  S # Display values
 }
-# Test
-sharpe.ratio(c("STLA", "UNM", "NVDA", "AAPL", "CVNA"), s = "2022-09-01")
+sharpe.ratio(c("STLA", "UNM", "NVDA", "AAPL", "CVNA"), s = "2026-01-01") # Test
