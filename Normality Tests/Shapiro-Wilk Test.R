0537c39b54f775821cf8eb0174fd1e68faeb4143
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-03-26 11:16:01 +0500

    Update Shapiro-Wilk Test.R
    Reduced unnecessary brackets and edited lines

--- Normality Tests/Shapiro-Wilk Test.R
+++ Normality Tests/Shapiro-Wilk Test.R
@@ -2,15 +2,15 @@
 t.shapiro <- function(x, lg = F, table = NULL){
   
   # Calculate logs and remove NA if needed
-  if (isTRUE(lg)){ x = diff(log(x))[-1,] }
+  if (lg) x = diff(log(x))[-1,]
   
   # For each column find values of Shapiro and store them into matrix
-  for (n in 1:ncol(x)){ table <- cbind(table, shapiro.test(x[,n])) }
+  for (n in 1:ncol(x)) table <- cbind(table, shapiro.test(x[,n]))
   
-    # Give it column names from original matrix
-    colnames(table) <- colnames(x)
+  # Give it column names from original matrix
+  colnames(table) <- colnames(x)
     
-    # Display values
-    return(table[1:(nrow(table)-2),])
+  # Display values
+  table[1:(nrow(table)-2),]
 }
 t.shapiro(stock_data, lg = T) # Test
